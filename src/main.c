dcccd3e51d8608858cbedb928154355bd237642b
syarin-x <magicspell.max.mix.mutax@gmail.com>
2022-04-30 22:19:19 +0900

    feat : compile integer.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include "mcc.h"
 
 int main(int argc, char **argv){
-    printf("Hello World\n");
+
+    if(argc != 2){
+        fprintf(stderr, "mcc: error: Invalid Argument num.");
+    }
+
+    printf(".intel_syntax noprefix\n");
+    printf(".globl main\n");
+    printf("main:\n");
+    printf("  mov rax, %d\n", atoi(argv[1]));
+    printf("  ret\n");
+
     return 0;
 }
