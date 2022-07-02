34d71c18d495760e62088affd4e60e71c9b71391
syarin-x <magicspell.max.mix.mutax@gmail.com>
2022-07-03 00:10:31 +0900

    Support memcpy(), memmove(), strcpy() to string.h.

---
+++ bin/stdlib/include/string.h
@@ -0,0 +1,9 @@
+#ifndef __STRING_INC_H__
+#define __STRING_INC_H__
+
+void memcpy(void* s1, void* s2, int n);
+void memmove(void* s1, void* s2, int n);
+char* strcpy(char* s1, char* s2);
+
+
+#endif
