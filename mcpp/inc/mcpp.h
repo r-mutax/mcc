02b2899807ce49128848e9c627b18a35381776c5
syarin-x <magicspell.max.mix.mutax@gmail.com>
2022-08-04 00:58:41 +0900

    Add Makefile for mcpp.

---
+++ mcpp/inc/mcpp.h
@@ -0,0 +1,6 @@
+#ifndef MCPP_INC_H
+#define MCPP_INC_H
+#include <stdio.h>
+
+
+#endif
---
+++ mcpp/src/main.c
@@ -0,0 +1,10 @@
+#include "mcpp.h"
+
+int main(int argc, char** argv){
+
+    if(argc < 2){
+        fprintf(stderr, "mcpp: error: Invalid Argument num.\n");
+    }
+
+    return 0;
+}
